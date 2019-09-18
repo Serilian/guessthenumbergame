c7a9690c5b18e764998143e5a00c392c35612147
fhagno@yahoo.com <fhagno@yahoo.com>
2019-09-18 15:40:02 +0200

    Adding spring boot to application

--- console/src/main/java/pl/filiphagno/Main.java
+++ console/src/main/java/pl/filiphagno/Main.java
@@ -1,19 +1,16 @@
 package pl.filiphagno;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import pl.filiphagno.config.GameConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @Slf4j
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         log.info("Welcome to the game");
-
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(GameConfig.class);
-
-        context.close();
+        SpringApplication.run(Main.class, args);
     }
 }
